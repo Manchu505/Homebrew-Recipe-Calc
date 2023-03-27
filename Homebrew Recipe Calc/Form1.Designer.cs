1c5128e2ee73ea772a9f9519ec71ec515624fb55
Manchu505 <19881829+Manchu505@users.noreply.github.com>
2023-03-27 08:34:17 -0500

    Add project files.

---
+++ Homebrew Recipe Calc/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Homebrew_Recipe_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "None",
+            "Wyeast 1007 - German Ale",
+            "Wyeast 1010 - American Wheat",
+            "Wyeast 1028 - London Ale",
+            "Wyeast 1056 - American Ale",
+            "Wyeast 1084 - Irish Ale",
+            "Wyeast 1098 - British Ale",
+            "Wyeast 1099 - Whitbread Ale",
+            "Wyeast 1187 - Ringwood Ale",
+            "Wyeast 1214 - Belgian Abbey",
+            "Wyeast 1272 - American Ale II",
+            "Wyeast 1275 - Thames Valley Ale",
+            "Wyeast 1318 - London Ale III",
+            "Wyeast 1332 - Northwest Ale",
+            "Wyeast 1335 - British Ale II",
+            "Wyeast 1388 - Belgian Strong Ale",
+            "Wyeast 1450 - Denny\'s Favorite 50",
+            "Wyeast 1469 - West Yorkshire Ale",
+            "Wyeast 1728 - Scottish Ale",
+            "Wyeast 1762 - Belgian Abbey II",
+            "Wyeast 1764 - Pacman",
+            "Wyeast 1968 - London ESB Ale",
+            "Wyeast 2000 - Budvar Lager",
+            "Wyeast 2001 - Urquell Lager",
+            "Wyeast 2007 - Pilsen Lager",
+            "Wyeast 2035 - American Lager",
+            "Wyeast 2042 - Danish Lager",
+            "Wyeast 2112 - California Lager",
+            "Wyeast 2124 - Bohemian Lager",
+            "Wyeast 2206 - Bavarian Lager",
+            "Wyeast 2247 - European Lager",
+            "Wyeast 2278 - Czech Pils",
+            "Wyeast 2308 - Munich Lager",
+            "Wyeast 2352 - Munich Lager II",
+            "Wyeast 2565 - Kolsch",
+            "Wyeast 2575 - Kolsch II",
+            "Wyeast 3068 - Weihenstephan Weizen",
+            "Wyeast 3333 - German Wheat",
+            "Wyeast 3463 - Forbidden Fruit",
+            "Wyeast 3522 - Belgian Ardennes",
+            "Wyeast 3538 - Leuven Pale Ale",
+            "Wyeast 3638 - Bavarian Wheat",
+            "Wyeast 3711 - French Saison",
+            "Wyeast 3724 - Belgian Saison",
+            "Wyeast 3725 - Bier de Garde",
+            "Wyeast 3726 - Farmhouse Ale",
+            "Wyeast 3787 - Trappist High Gravity",
+            "Wyeast 3942 - Belgian Wheat",
+            "Wyeast 3944 - Belgian Witbeer",
+            "Wyeast 5112 - Brettanomyces brux",
+            "Wyeast 5151 - Brett Claussenii",
+            "Wyeast 5526 - Brettanomyces Lambicus",
+            "WLP001 - California Ale",
+            "WLP002 - English Ale",
+            "WLP003 - German Ale II",
+            "WLP004 - Irish Ale",
+            "WLP005 - British Ale",
+            "WLP006 - Bedford British Ale",
+            "WLP007 - Dry English Ale",
+            "WLP008 - East Coast Ale",
+            "WLP009 - Australian Ale",
+            "WLP013 - London Ale",
+            "WLP023 - Burton Ale",
+            "WLP025 - Southwold Ale",
+            "WLP028 - Edinburgh Scottish Ale",
+            "WLP029 - German Ale/Kolsch",
+            "WLP036 - Dusseldorf Alt",
+            "WLP039 - East Midlands Ale",
+            "WLP051 - California Ale V",
+            "WLP300 - Hefeweizen",
+            "WLP320 - American Hefeweizen",
+            "WLP351 - Bavarian Weizen",
+            "WLP380 - Hefeweizen IV",
+            "WLP400 - Belgian Wit",
+            "WLP410 - Belgian Wit II",
+            "WLP500 - Trappist Ale",
+            "WLP530 - Abbey Ale",
+            "WLP540 - Abbey Ale IV",
+            "WLP550 - Belgian Ale",
+            "WLP565 - Belgian Saison I",
+            "WLP570 - Belgian Golden Ale",
+            "WLP575 - Belgian Style Ale Yeast Blend",
+            "WLP603 - Torulaspora delbrueckii",
+            "WLP644 - Brett Brux var Trois",
+            "WLP800 - Pilsner Lager",
+            "WLP810 - San Francisco Lager",
+            "WLP820 - Octoberfest/Märzen",
+            "WLP830 - German Lager",
+            "WLP838 - S. German Lager",
+            "WLP840 - American Lager",
+            "WLP920 - Old Bavarian Lager",
+            "ECY08 - Saison Brasserie Blend",
+            "BKY0C3 - Cantillon Iris 07 Brett B 3",
+            "EOS01 - Carol House Blend",
+            "EOS02 - ECY24 nanus + JY87 Chateux",
+            "EOS03 - ECY24 nanus + EOS01 Carol",
+            "Yeast Bay Sigmund\'s Voss Kveik",
+            "Coopers Ale (Dry)",
+            "Danstar Munich (Dry)",
+            "Danstar Munich Classic (Dry)",
+            "Danstar Nottingham (Dry)",
+            "Danstar Windsor (Dry)",
+            "Imperial A04 - Barbarian",
+            "Imperial A20 - Citrus",
+            "Imperial B45 - Gnome",
+            "Imperial A38 - Juice",
+            "Imperial B64 - Napoleon",
+            "Lallemand BRY-97",
+            "Lallemand Voss Kveik",
+            "Mangrove Jack\'s M27-Belgian Ale (Dry)",
+            "Mangrove Jack\'s-M41 (Belgian Ale, Dry)",
+            "Muntons Ale (Dry)",
+            "Cellar Science Germany",
+            "Cellar Science Berlin",
+            "Safale US-05 (Dry)",
+            "Safale S-04 (Dry)",
+            "Safbew T-58 (Dry)",
+            "Safbrew S-33 (Dry)",
+            "Safbrew WB-06 (Dry)",
+            "Saflager S-23 (Dry)",
+            "Safale K-97 (Dry)",
+            "Safale BE-134",
+            "Saflager W-34/70 (Dry)"});
+            this.listBox1.Location = new System.Drawing.Point(85, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(321, 69);
+            this.listBox1.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.tableLayoutPanel1.ColumnCount = 16;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 2);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(67, 171);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 10;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(902, 272);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(3, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(34, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(3, 63);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(34, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(53, 3);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(194, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(53, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(33, 319);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(28, 240);
+            this.panel1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(33, 171);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 149);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 622);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
